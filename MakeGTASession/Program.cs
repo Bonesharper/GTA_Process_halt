e6decb6f9e331547a0cb40bb273106a42684e5b2
Bonesharper <skrivanek.jenda@gmail.com>
2020-02-19 19:25:14 +0100

    init

---
+++ MakeGTASession/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeGTASession
+{
+    public static class Program
+    {
+        public static Process process;
+
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler Handler, bool add);
+        private delegate bool EventHandler(CtrlType sig);
+        private static EventHandler _handler;
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+        enum CtrlType
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+        private static bool Handler(CtrlType sig)
+        {
+            switch (sig)
+            {
+                case CtrlType.CTRL_C_EVENT:
+                case CtrlType.CTRL_LOGOFF_EVENT:
+                case CtrlType.CTRL_SHUTDOWN_EVENT:
+                case CtrlType.CTRL_CLOSE_EVENT:
+                    Program.Resume(process);
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+        public static void Main(string[] args)
+        {
+            process = Process.GetProcessesByName("GTA5").FirstOrDefault();
+            if (process == null) return;
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+            Suspend(process);
+            for (var i = 8; i>0; i--)
+            {
+                Console.WriteLine($"Process GTA5 suspended for {i} seconds ...");
+                System.Threading.Thread.Sleep(1000);
+            }            
+            Resume(process);
+            
+            Console.WriteLine("Process resumed. Press any key to close this window.");
+            Console.ReadKey();                        
+        }
+
+        public static void Suspend(Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {                
+                var pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                
+                SuspendThread(pOpenThread);
+            }
+        }
+        public static void Resume(this Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {
+                var pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                ResumeThread(pOpenThread);
+            }
+        }               
+    }
+}
